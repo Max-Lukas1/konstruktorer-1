3f90c2c9fdf06f49d1ff294cdb587d8a40ae8b2e
Max-Lukas1 <max-lukas@outlook.com>
2024-04-03 10:37:48 +0200

    Add project files.

---
+++ konstruktorer 1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bilinformation
+{
+    internal class bilinformation
+    {
+        public string modell;
+        public int ålder;
+        public string tillverknings_datum;
+        public string tillverkare;
+        public int priss;
+
+        public bilinformation(string m, int å, string td, string t, int p)
+        {
+            modell = m;
+            ålder = å;
+            tillverknings_datum = td;
+            tillverkare = t;
+            priss = p; 
+        }
+
+    }
+}
---
+++ konstruktorer 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace bilinformation
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            bilinformation p = new bilinformation("v60", 20, "28 april 2002", "volvo", 20000);
+
+            Console.WriteLine(p.modell);
+            Console.WriteLine(p.ålder);
+            Console.WriteLine(p.tillverknings_datum);
+            Console.WriteLine(p.tillverkare);
+            Console.WriteLine(p.priss);
+        }
+    }
+}
